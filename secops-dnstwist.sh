2b28d36f9a3d72cefa279234772544c17b925428
lknittig-panw <lknittig@paloaltonetworks.com>
2019-10-04 08:51:15 -0700

    updated edl

--- secops-dnstwist.sh
+++ secops-dnstwist.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-# Ask the user for their name
-=======
 # Ask the user for their domainname
->>>>>>> 0ce618a271397823c919daaed0f6e2c4d46ca652
 echo enter domainname:
 read domainname
 
